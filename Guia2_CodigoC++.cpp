9ec3de997fd38c767be2bf49a6be8bd344165558
JesusG. <condorigustavo45@gmail.com>
2024-03-29 01:13:33 -0500

    Actividad 4:1

--- Guia2_CodigoC++.cpp
+++ Guia2_CodigoC++.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+using namespace std;
 
 //ACTIVIDADES
 //ACTIVIDAD 1.
@@ -9,3 +11,47 @@
 
 //ACTIVIDAD 4.
 
+    //1.-
+class Libro{
+    string autor;
+    string titulo;
+    bool prestam;
+    public:
+    Libro(string a, string t){
+        this->autor=a;
+        this->titulo=t;
+    }
+        string getAutor(){
+            return autor;
+        }
+        string getTitulo(){
+            return titulo;
+        }
+        void setPrestam(bool p){
+            prestam=p;
+        }
+        void del_prestam(){
+            cout<<"Se devolvio el libro"<<endl;
+            prestam=false;
+        }
+    
+};
+class Biblioteca{
+    public:
+    bool Presta_libro( Libro libro){
+        cout<<"Se presto el libro "<<libro.getTitulo()<<" de "<<libro.getAutor()<<endl;
+        libro.setPrestam(true);
+        return true;
+    }
+};
+
+int main(){
+    Libro libro1("Miguel De Cervantes","Don Quijote de la Mancha");
+    Biblioteca bl;
+    bl.Presta_libro(libro1);
+    libro1.del_prestam();
+
+    return 0;
+}
+
+   //2.- 
